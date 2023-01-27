9dd144f24842d798da7de2abdc41d9e7a48b1875
Pro100Vlad1047 <91087864+Pro100Vlad1047@users.noreply.github.com>
2023-01-27 22:24:26 +0200

    Add files via upload

--- TicTakToe/JS/api.js
+++ TicTakToe/JS/api.js
@@ -5,15 +5,9 @@ let posTwo;
 let gameStarter=0;
 let gameWinner=0;
 let q=0;
-
-let posOneArr=[
-
-];
-
-let posTwoArr=[
-	
-];
-
+let posOneArr=[];
+let posTwoArr=[];
+let rainbow;
 let winnerPositions=[
 	[1,2,3],
 	[4,5,6],
@@ -25,10 +19,7 @@ let winnerPositions=[
 	[3,5,7],
 ];
 
-
-
-
-
+/////////////////////////////////////////////////////////////////////////
 
 function playerTurn(e){
 	if(e.textContent==""&& gameStarter==0){
@@ -45,19 +36,14 @@ function playerTurn(e){
 			checkEquals(posTwoArr);
 			turn++;
 		}
-		//console.log(`posOne is `+posOneArr);
-		//console.log(`posTwo is `+posTwoArr);
 	}
 }
-
-
 
 
 function checkEquals(posArrays){
 	posArrays.sort(function(a, b){return a - b});
 	checkTwoMassives(posArrays);
 	if(gameWinner==1){
-		console.log(`Winner is ${posArrays}::::${turn}`);
 		gameStarter++;
 		if(turn%2==0){
 			document.querySelector("#text_message").innerHTML="Winner is X";
@@ -71,33 +57,16 @@ function checkEquals(posArrays){
 }
 
 
-function restartGame(){
-	turn=0;
-	posTwoArr=[];
-	posOneArr=[];
-	gameStarter=0;
-	gameWinner=0;
-	console.log(document.querySelectorAll(".plates").innerHTML);
-	for(let i=1;i<10;++i){
-		document.getElementById(i).innerHTML="";
-		console.log(document.getElementById(i).innerHTML);
-	}
-	document.querySelector("#text_message").innerHTML="Let`s go to play!";
-}
-
-
-
 function checkTwoMassives(posArrays){
 	let j=0;
-	for(let i=0;i<winnerPositions.length;++i){// 3posArrays to All winnerPositions numbers
-		// cut a blocks of big array
-		for(let k=0;k<5;++k){ // 3posArrays to 3 winnerPositions numbers
-			for(let w=0;w<3;++w){///// 1posArray to 3winnerPositions
+	for(let i=0;i<winnerPositions.length;++i){
+		for(let k=0;k<5;++k){ 
+			for(let w=0;w<3;++w){
 				if(posArrays[k]==winnerPositions[j][w]){
 					++q;
 					if(q>=3){
 						gameWinner=1;
-
+						rainbowing();
 					}
 				}
 			}	
@@ -105,4 +74,31 @@ function checkTwoMassives(posArrays){
 		q=0;
 		++j;	
 	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+function restartGame(){
+	turn=0;
+	posTwoArr=[];
+	posOneArr=[];
+	gameStarter=0;
+	gameWinner=0;
+	clearInterval(rainbow);
+	document.querySelector("#text_message").style.color="#a9ff00";
+	for(let i=1;i<10;++i){
+		document.getElementById(i).innerHTML="";
+	}
+	document.querySelector("#text_message").innerHTML="Let`s go to play!";
+}
+
+function rainbowing(){
+		rainbow=setInterval(function(){
+		if(document.querySelector("#text_message").style.color=="red"){
+			document.querySelector("#text_message").style.color="yellow";
+		}else if(document.querySelector("#text_message").style.color=="yellow"){
+			document.querySelector("#text_message").style.color="#a9ff00";
+		}else if(document.querySelector("#text_message").style.color="#a9ff00"){
+			document.querySelector("#text_message").style.color="red";
+		}
+	},300);
 }
